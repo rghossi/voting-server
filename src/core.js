5c8216aa6ec9db08241966542ca78376af74b4dc
rghossi <rafael.ghossi@gmail.com>
2017-02-08 13:56:52 -0200

    moving to the next pair and ending the vote

--- src/core.js
+++ src/core.js
@@ -5,11 +5,17 @@ export function setEntries(state, entries) {
 }
 
 export function next(state) {
-  const entries = state.get('entries');
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  const entries = state.get('entries').concat(getWinners(state.get('vote')));;
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+	  return state.merge({
+	    vote: Map({pair: entries.take(2)}),
+	    entries: entries.skip(2)
+	  });
+  }
 }
 
 export function vote(state, entry) {
