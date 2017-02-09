8d6bd86eb9de00e3c806656583b3193633342907
rghossi <rafael.ghossi@gmail.com>
2017-02-09 01:18:45 -0200

    piece of code i forgot to type/paste :(

--- src/core.js
+++ src/core.js
@@ -4,6 +4,16 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes)  return [a];
+  else if (aVotes < bVotes)  return [b];
+  else                       return [a, b];
+}
+
 export function next(state) {
   const entries = state.get('entries').concat(getWinners(state.get('vote')));;
   if (entries.size === 1) {
